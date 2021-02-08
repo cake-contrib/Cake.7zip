241753e2dc46e2fc240025dfccbaae048b0c8906
Nils Andresen <nils@nils-andresen.de>
2021-02-08 20:42:39 +0100

    (GH-108) added documentation for Namespaces

---
+++ src/Cake.7zip/Namespaces.cs
@@ -0,0 +1,79 @@
+using System.Runtime.CompilerServices;
+
+// This file enables generation of namespace documentation,
+// see https://cakebuild.net/community/contributing/documentation#namespaces.
+#pragma warning disable SA1403 // File may only contain a single namespace
+#pragma warning disable SA1649 // File name should match first type name
+#pragma warning disable SA1402 // File may only contain a single type
+
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+namespace Cake.SevenZip
+{
+    /// <summary>
+    /// This namespace contain types related to running 7zip.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
+
+namespace Cake.SevenZip.Arguments
+{
+    /// <summary>
+    /// This namespace contain types related to running 7zip.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
+
+namespace Cake.SevenZip.Builder
+{
+    /// <summary>
+    /// This namespace contain types related to running 7zip.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
+
+namespace Cake.SevenZip.Commands
+{
+    /// <summary>
+    /// This namespace contain types related to running 7zip.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
+
+namespace Cake.SevenZip.Parsers
+{
+    /// <summary>
+    /// This namespace contain types related to running 7zip.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
+
+namespace Cake.SevenZip.Switches
+{
+    /// <summary>
+    /// This namespace contain types related to running 7zip.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
+
+#pragma warning restore SA1403 // File may only contain a single namespace
+#pragma warning restore SA1649 // File name should match first type name
+#pragma warning restore SA1402 // File may only contain a single type
